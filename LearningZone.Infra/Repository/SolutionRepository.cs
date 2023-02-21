f0294818f1c0baa9fb644134a49cd8717d83ba80
BushraKhattabAlawneh <bkalawneh19@cit.just.edu.jo>
2023-02-21 23:31:29 +0300

    z

---
+++ LearningZone.Infra/Repository/SolutionRepository.cs
@@ -0,0 +1,11 @@
+﻿using LearningZone.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningZone.Infra.Repository
+{
+    public class SolutionRepository: ISolutionRepository
+    {
+    }
+}
