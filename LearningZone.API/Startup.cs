8ed1721514b13af2186ccb806a8f231d7b187caa
BushraKhattabAlawneh <bkalawneh19@cit.just.edu.jo>
2023-02-21 22:47:24 +0300

    z

--- LearningZone.API/Startup.cs
+++ LearningZone.API/Startup.cs
@@ -58,6 +58,11 @@ namespace LearningZone.API
                     };
                 });
 
+            services.AddControllers();
+            services.AddScoped<IAboutusRepository, AboutusRepository>();
+            services.AddScoped<IAboutusService, AboutusService>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
