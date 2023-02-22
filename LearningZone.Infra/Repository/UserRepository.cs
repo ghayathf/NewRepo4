60d3fa12fd57fad273ffb6757492592c10da26e9
raneemOb <rmobeidat197@cit.just.edu.jo>
2023-02-23 02:12:21 +0300

    r

--- LearningZone.Infra/Repository/UserRepository.cs
+++ LearningZone.Infra/Repository/UserRepository.cs
@@ -26,7 +26,7 @@ namespace LearningZone.Infra.Repository
             p.Add("PhoneNum", user.Phonenumber, dbType: DbType.String, direction: ParameterDirection.Input);
             p.Add("FName", user.Firstname, dbType: DbType.String, direction: ParameterDirection.Input);
             p.Add("LName", user.Lastname, dbType: DbType.String, direction: ParameterDirection.Input);
-            p.Add("UROLE_ID", user.RoleId, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            p.Add("UROLE_ID", user.Role_Id, dbType: DbType.Int32, direction: ParameterDirection.Input);
             var result = dbContext.Connection.Execute("Final_User_Package.CREATEUser", p, commandType: CommandType.StoredProcedure);
         }
         //(UName IN Final_User.UserName%TYPE,UPassword IN Final_User.UserPassword%TYPE,UEmail IN Final_User.Email%TYPE,PhoneNum IN Final_User.PhoneNumber%TYPE,FName IN Final_User.FirstName%TYPE,LName IN Final_User.LastName%TYPE,UROLE_ID IN Final_User.ROLE_ID%TYPE);
@@ -65,7 +65,7 @@ namespace LearningZone.Infra.Repository
             p.Add("PhoneNum", user.Phonenumber, dbType: DbType.String, direction: ParameterDirection.Input);
             p.Add("FName", user.Firstname, dbType: DbType.String, direction: ParameterDirection.Input);
             p.Add("LName", user.Lastname, dbType: DbType.String, direction: ParameterDirection.Input);
-            p.Add("UROLE_ID", user.RoleId, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            p.Add("UROLE_ID", user.Role_Id, dbType: DbType.Int32, direction: ParameterDirection.Input);
             var result = dbContext.Connection.Execute("Final_User_Package.UpdateUser", p, commandType: CommandType.StoredProcedure);
         }
     }
