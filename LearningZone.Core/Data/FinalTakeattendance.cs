2ac7837aebb8f2bdde2e676d52256cfb4a0f1e89
GhayathQuraan <ghayath04@hotmail.com>
2023-02-24 23:23:28 +0300

    ghayath

---
+++ LearningZone.Core/Data/FinalTakeattendance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LearningZone.Core.Data
+{
+    public partial class FinalTakeattendance
+    {
+        public decimal Takeattendanceid { get; set; }
+        public DateTime? Attendancedate { get; set; }
+        public decimal? Tsid2 { get; set; }
+        public decimal? Attendid { get; set; }
+    }
+}
