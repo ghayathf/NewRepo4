78f640c66af840f89415b4a58dc3056c8c5ab7c8
Ahmadghazal1 <74017377+Ahmadghazal1@users.noreply.github.com>
2023-03-25 06:08:15 +0300

    xx

--- LearningZone.API/Controllers/TaskController.cs
+++ LearningZone.API/Controllers/TaskController.cs
@@ -2,7 +2,9 @@
 using LearningZone.Core.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace LearningZone.API.Controllers
@@ -48,6 +50,26 @@ namespace LearningZone.API.Controllers
        public void DeleteTask(int id)
         {
             _taskService.DeleteTask(id);
+        }
+
+
+        [Route("UploadTask")]
+        [HttpPost]
+        public FinalTask UploadMaterial()
+        {
+            var file = Request.Form.Files[0];
+            var fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var fullpath = Path.Combine("C:\\LearningHub_Angular\\src\\assets\\HomeAssets\\Task", fileName);
+
+            using (var stream = new FileStream(fullpath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            FinalTask item = new FinalTask();
+            item.Taskfile = fileName;
+
+            return item;
         }
     }
 }
